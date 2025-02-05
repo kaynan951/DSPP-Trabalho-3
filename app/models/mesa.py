1128b9fd544e7bb066e39d2758346bc0bb83577c
LGuilhermeMoreira <Lguilhermemoreiraleite@gmail.com>
2025-02-05 18:53:14 -0300

    update

--- app/models/mesa.py
+++ app/models/mesa.py
@@ -7,16 +7,15 @@ class Mesa(BaseModel):
     capacidade: int = Field(ge=1)
     localizacao: str = Field(min_length=3)
     status: str = Field(default='disponivel')
-    clientes : Optional[List["Cliente"]] = Field(default_factory=list)
 
 
 class MesaCreate(Mesa):
     pass
 
 class MesaUpdate(Mesa):
-    id: Optional[str]  
     numero: Optional[int]  
     capacidade: Optional[int]  
     localizacao: Optional[str]  
-    status: Optional[str]  
+    status: Optional[str]
+    clientes: Optional[List["Cliente"]] 
     
